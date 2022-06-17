b9fe61218f270ab5e629911f634a627c27af38a5
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2022-06-17 14:05:14 +0530

    Create 28. Triplet sum in array.cpp
    this cide having tc o(n^2) and space comp O(n).

---
+++ Array/28. Triplet sum in array.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+           int ans=0;
+    sort(A, A + n);
+    //bool true,false;
+    
+    for(int i=0;i<n-2;i++)
+    {
+        int j=i+1;
+        int k=n-1;
+        while(j<k)
+        {
+            if(A[i]+A[j]+A[k]==X) 
+            {
+            return(true);
+            }
+            else if(A[i]+A[j]+A[k]<X) j++;
+            else k--;
+        }
+        //if(ans==1) break;
+        
+    }
+    return(false);
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+  // } Driver Code Ends
