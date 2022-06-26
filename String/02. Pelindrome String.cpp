951a14026f08eb9e97be7943d46d8792e3d34315
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2022-06-26 08:23:31 +0530

    Create 02. Pelindrome String.cpp

---
+++ String/02. Pelindrome String.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int n = S.length();
+	    int c=0;
+	    for(int i=0,j=n-1;i<j;i++,j--)
+	    {
+	        if(S[i]!=S[j])
+	        {
+	        c++;
+	        }
+	    }
+	    if(c==0) return(1);
+	    else return(0);
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
