94be752de8a5a865740528a882651631498df345
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2023-02-08 07:09:46 +0530

    Update Max Sum Sub arr Kadanes Algo.cpp

--- Max Sum Sub arr Kadanes Algo.cpp
+++ Max Sum Sub arr Kadanes Algo.cpp
@@ -54,67 +54,47 @@ int main()
 
 
 
-
-import java.io.IOException;
-
-public class ExceptionHandlingExample {
-  public static void main(String[] args) {
-    try {
-      divideByZero();
-    } catch (ArithmeticException ex) {
-      System.out.println("Caught ArithmeticException in main method.");
-    } catch (Exception ex) {
-      System.out.println("Caught Exception in main method.");
-    } finally {
-      System.out.println("Finally block in main method.");
+public class Main {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
     }
-
-    tryWithMultipleCatches();
-    tryWithFinally();
-    tryWithNestedTry();
-  }
-
-  public static void divideByZero() throws ArithmeticException {
-    int x = 10, y = 0;
-    int result = x / y;
-    System.out.println("Result: " + result);
-  }
-
-  public static void tryWithMultipleCatches() {
-    try {
-      int[] arr = new int[5];
-      arr[10] = 100;
-    } catch (ArithmeticException ex) {
-      System.out.println("Caught ArithmeticException in tryWithMultipleCatches.");
-    } catch (ArrayIndexOutOfBoundsException ex) {
-      System.out.println("Caught ArrayIndexOutOfBoundsException in tryWithMultipleCatches.");
-    } finally {
-      System.out.println("Finally block in tryWithMultipleCatches.");
-    }
-  }
-
-  public static void tryWithFinally() {
-    try {
-      throw new IOException();
-    } catch (IOException ex) {
-      System.out.println("Caught IOException in tryWithFinally.");
-    } finally {
-      System.out.println("Finally block in tryWithFinally.");
-    }
-  }
-
-  public static void tryWithNestedTry() {
-    try {
-      try {
-        throw new NullPointerException();
-      } catch (NullPointerException ex) {
-        System.out.println("Caught NullPointerException in nested try block.");
-        throw ex;
-      }
-    } catch (NullPointerException ex) {
-      System.out.println("Caught NullPointerException in outer try block.");
-    } finally {
-      System.out.println("Finally block in tryWithNestedTry.");
-    }
-  }
 }
+
+
+
+class Main {
+    static void throwException() throws Exception {
+        throw new Exception("My Exception");
+    }
+
+    public static void main(String[] args) {
+        try {
+            throwException();
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
+
+
+
+
+
+class Main {
+    static void throwException() {
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        throwException();
+    }
+}
+
