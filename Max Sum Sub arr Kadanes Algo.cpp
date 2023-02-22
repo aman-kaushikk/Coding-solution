3ac957eed89b517308d484d6f8831413cd9e043f
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2023-02-22 22:42:47 +0530

    Update Max Sum Sub arr Kadanes Algo.cpp

--- Max Sum Sub arr Kadanes Algo.cpp
+++ Max Sum Sub arr Kadanes Algo.cpp
@@ -53,6 +53,56 @@ int main()
 
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class DataInOut {
+
+    public static void main(String[] args) {
+        try {
+            // Create a new PrintWriter instance to write to a file named "data.txt"
+            PrintWriter writer = new PrintWriter(new File("data.txt"));
+
+            // Read data from the console in different data types and write it to the file
+            Scanner console = new Scanner(System.in);
+
+            System.out.print("Enter an integer: ");
+            int num = console.nextInt();
+            writer.println(num);
+
+            System.out.print("Enter a double: ");
+            double d = console.nextDouble();
+            writer.println(d);
+
+            System.out.print("Enter a string: ");
+            String str = console.next();
+            writer.println(str);
+
+            // Close the PrintWriter
+            writer.close();
+
+            // Read data from the file using Scanner
+            Scanner fileScanner = new Scanner(new File("data.txt"));
+
+            int numFromFile = fileScanner.nextInt();
+            double dFromFile = fileScanner.nextDouble();
+            String strFromFile = fileScanner.next();
+
+            // Print the data that was read from the file
+            System.out.println("Data from file:");
+            System.out.println("Integer: " + numFromFile);
+            System.out.println("Double: " + dFromFile);
+            System.out.println("String: " + strFromFile);
+
+            // Close the Scanner
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
 
