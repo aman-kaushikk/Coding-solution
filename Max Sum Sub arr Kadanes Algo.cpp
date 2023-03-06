e6fd842c2e789fac0ef032247a0adc5b8658d15e
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2023-03-06 23:48:03 +0530

    Update Max Sum Sub arr Kadanes Algo.cpp

--- Max Sum Sub arr Kadanes Algo.cpp
+++ Max Sum Sub arr Kadanes Algo.cpp
@@ -94,5 +94,123 @@ int main()
 
 
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample Form</title>
+	<!-- Adding Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<h2>Sample Form</h2>
+		<form>
+			<div class="form-group">
+				<label for="input-field">Input Field:</label>
+				<input type="text" class="form-control" id="input-field">
+				<label for="display-field">Display Field:</label>
+				<label class="form-control" id="display-field"></label>
+			</div>
+			<div class="form-group">
+				<label for="drop-down">Drop Down:</label>
+				<select class="form-control" id="drop-down">
+					<option value="">--Select--</option>
+				</select>
+				<!-- Adding tooltip -->
+				<span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-placement="right" title="" id="tooltip"></span>
+			</div>
+			<div class="form-group">
+				<label for="date-field">Date Field:</label>
+				<input type="text" class="form-control" id="date-field" readonly>
+			</div>
+			<div class="form-group">
+				<label>Checkbox:</label>
+				<input type="checkbox" id="checkbox">
+				<label for="yes-radio">Yes</label>
+				<input type="radio" name="radio" id="yes-radio">
+				<label for="no-radio">No</label>
+				<input type="radio" name="radio" id="no-radio" checked>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+	<!-- Adding jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<!-- Adding Bootstrap JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script>
+		// Adding values to drop-down using jQuery
+		$(document).ready(function(){
+			$('#drop-down').append($('<option>', {
+				value: 'value1',
+				text: 'Value 1'
+			}));
+			$('#drop-down').append($('<option>', {
+				value: 'value2',
+				text: 'Value 2'
+			}));
+			$('#drop-down').append($('<option>', {
+				value: 'value3',
+				text: 'Value 3'
+			}));
+		});
+
+		// Displaying selected value in tooltip
+		$(document).ready(function(){
+			$('#drop-down').change(function(){
+				$('#tooltip').attr('title', $(this).val()).tooltip('fixTitle');
+			});
+		});
+
+		// Hiding date picker and enabling text field
+		$(document).ready(function(){
+			$('#date-field').datepicker({
+				autoclose: true,
+				todayHighlight: true
+			}).on('show', function(e){
+				e.preventDefault();
+				e.stopPropagation();
+			});
+			$('#date-field').click(function(){
+				$('#date-field').datepicker('show');
+			}).on('change', function(){
+				$('#date-field').datepicker('hide');
+			});
+			$('#date-field').focus(function(){
+				$('.datepicker').hide();
+			});
+		});
+
+		
+
+
+
+
+
+$(document).ready(function(){
+			// Default values
+			$('#no-radio').prop('checked', true);
+			$('#checkbox').prop('checked', false);
+
+			// Checkbox click event
+			$('#checkbox').click(function(){
+				if($(this).prop('checked')){
+					$('#yes-radio').prop('checked', true);
+				}else{
+					$('#no-radio').prop('checked', true);
+				}
+			});
+
+			// Radio button click event
+			$('input[name="radio"]').click(function(){
+				if($('#yes-radio').prop('checked')){
+					$('#checkbox').prop('checked', true);
+				}else{
+					$('#checkbox').prop('checked', false);
+				}
+			});
+		});
+
+
 
 
