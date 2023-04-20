316a6e5fdb3126cb06de3a957d60e574ce68df4e
aman-kaushikk <64920910+aman-kaushikk@users.noreply.github.com>
2023-04-20 09:30:18 +0530

    Update Max Sum Sub arr Kadanes Algo.cpp

--- Max Sum Sub arr Kadanes Algo.cpp
+++ Max Sum Sub arr Kadanes Algo.cpp
@@ -53,104 +53,26 @@ int main()
 
 
 
-<label for="range-slider">Range Slider:</label>
-<input type="range" id="range-slider" min="0" max="100" value="50">
-<input type="text" id="range-value" value="50">
 
+<form method="POST" action="upload.jsp" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="Upload">
+</form>
 
-#range-slider {
-  width: 100%;
-  height: 20px;
-  margin-bottom: 20px;
-}
+<%
+    // Get the uploaded file
+    Part filePart = request.getPart("file");
+    InputStream fileContent = filePart.getInputStream();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(fileContent));
 
-#range-value {
-  width: 50px;
-  margin-left: 10px;
-}
-
-
-
-var rangeSlider = document.getElementById("range-slider");
-var rangeValue = document.getElementById("range-value");
-
-rangeSlider.oninput = function() {
-  rangeValue.value = this.value;
-}
-
-rangeValue.oninput = function() {
-  rangeSlider.value = this.value;
-}
-
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-
-
-$(document).ready(function() {
-  $('#emi-form').submit(function(event) {
-    event.preventDefault();
-    
-    var formData = {
-      principle: $('#principle').val(),
-      rate: $('#rate').val(),
-      tenure: $('#tenure').val()
-    };
-    
-    $.ajax({
-      type: 'POST',
-      url: '/calculate-emi',
-      data: JSON.stringify(formData),
-      contentType: 'application/json',
-      success: function(result) {
-        $('#emi-result').text('EMI: ' + result.emi);
-      }
-    });
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var pageCounter = 1;
-var animalContainer = document.getElementById("animal-info");
-var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
-  ourRequest.onload = function() {
-    if (ourRequest.status >= 200 && ourRequest.status < 400) {
-      var ourData = JSON.parse(ourRequest.responseText);
-      renderHTML(ourData);
-    } else {
-      console.log("We connected to the server, but it returned an error.");
+    // Read the contents of the file
+    String line;
+    while ((line = reader.readLine()) != null) {
+        out.println(line);
     }
-    
-  };
 
-  ourRequest.onerror = function() {
-    console.log("Connection error");
-  };
-
-  ourRequest.send();
-  pageCounter++;
-  if (pageCounter > 3) {
-    btn.classList.add("hide-me");
-  }
-});
+    // Close the reader
+    reader.close();
+%>
 
 
